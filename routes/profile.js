ab2380d601430da974b42a848f12dd853f85b253
rajantara <rajanpensas@gmail.com>
2020-08-13 02:12:05 +0800

    add profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,19 +1,45 @@
 var express = require('express');
 var router = express.Router();
 var helpers = require('../helpers/util');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 /* GET home page. */
 module.exports = (db) => {
 
   router.get('/', helpers.isLoggedIn, function (req, res, next) {
-    db.query()
-    res.render('profile/view')
+    let users = req.session.users
+    console.log(users)
+    let sql = `SELECT * FROM users WHERE email = '${users.email}'`
+    db.query(sql, (err, data) => {
+      console.log(sql)
+      let result = data.rows[0]
+      if (err) return res.send(err)
+      res.render('profile/listProfile', {
+        title: 'edit Profile',
+        result,
+        url : 'profile',
+        users
+      })
+    })
+    
   });
-
-  router.post('/', helpers.isLoggedIn, function (req, res, next) {
-    res.redirect('/profile')
-  });
+    
+  //update data profile
+  router.post('/', helpers.isLoggedIn, (req, res) => {
+    const {password, firstname, lastname, position, job } = req.body;
+    let users =req.session.users;
+    bcrypt.hash(password,saltRounds, function (err, hash) {
+      if (err) return res.send(err)
+      let sql = `UPDATE users SET password = ${hash}',firstname ='${firstname}',lastname = '${lastname}',postion = '${position}', isfulltime='${job == 'Full Time' ? 'Full Time' : 'Part Time'}' WHERE email = '${users.email}'`;
+      console.log(sql)
+      db.query(sql, (err) => {
+        if (err) return res.send(err);
+        res.redirect('/projects')
+      }) 
+    })
+  })
   
 
   return router;
